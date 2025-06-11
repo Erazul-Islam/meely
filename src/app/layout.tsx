e5a45727033f7bee074bbfa39963ae298a5d2523
Erazul Islam <erazul89@gmail.com>
2025-06-11 20:27:29 +0600

    add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import {  Figtree } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/shared/navbar";
 const figtree = Figtree({
   variable: "--font-figtree-sans",
   subsets: ["latin"],
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body
         className={`${figtree.variable} antialiased`}
       >
+        <Navbar/>
         {children}
       </body>
     </html>

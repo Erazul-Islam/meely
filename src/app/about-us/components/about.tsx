13b699358c41919ab88cafb36d766ce1b4388fc1
Erazul Islam <erazul89@gmail.com>
2025-06-12 12:33:31 +0600

    add review and super app

--- src/app/about-us/components/about.tsx
+++ src/app/about-us/components/about.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import AboutHero from './about-hero';
 import MissionVission from './mission-vision';
 import Story from './story';
+import Review from '@/components/home/review';
+import SuperApp from '@/components/home/super-app';
 
 const About = () => {
     return (
@@ -9,6 +11,8 @@ const About = () => {
             <AboutHero/>
             <MissionVission/>
             <Story/>
+            <Review/>
+            <SuperApp/>
         </div>
     );
 };

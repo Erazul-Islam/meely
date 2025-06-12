a58492d9e89484350d3a6b2fe366d9c16bedff87
Erazul Islam <erazul89@gmail.com>
2025-06-12 12:29:11 +0600

    add story section

--- src/app/about-us/components/about.tsx
+++ src/app/about-us/components/about.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutHero from './about-hero';
 import MissionVission from './mission-vision';
+import Story from './story';
 
 const About = () => {
     return (
         <div>
             <AboutHero/>
             <MissionVission/>
+            <Story/>
         </div>
     );
 };
